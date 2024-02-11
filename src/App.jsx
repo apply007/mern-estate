feea097d1101f64c7e6f076717757e6751d86176
apply007 <hasanat153@gmail.com>
2024-02-11 13:30:20 +0600

    RoutesAll
    RoutesAll

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import About from './pages/About'
 export default function App() {
   return (
     <BrowserRouter>
+    //
     <Routes> 
 <Route path='/' element={<Home/>}></Route>
 <Route path='/profile' element={<Profile/>}></Route>
