24beae74bcbbd6a93794e635899cda2b7889d47f
apply007 <hasanat153@gmail.com>
2024-02-11 18:35:46 +0600

    New Routes comCreate Header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (
+    // 
     <BrowserRouter>
-    
+    <Header/>
     <Routes> 
 <Route path='/' element={<Home/>}></Route>
 <Route path='/profile' element={<Profile/>}></Route>
 <Route path='/sign-in' element={<SignIn/>}></Route>
 <Route path='/sign-up' element={<SignUp/>}></Route>
 <Route path='/about' element={<About/>}></Route>
- //
+ 
     </Routes>
     </BrowserRouter>
   )
