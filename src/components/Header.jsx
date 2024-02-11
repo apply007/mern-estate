e79519a9d2c72947777ce2f63079f4a3350c9aa6
apply007 <hasanat153@gmail.com>
2024-02-11 18:37:58 +0600

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,6 +28,7 @@ export default function Header() {
             <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
             </li>
+             
           </Link>
           <Link to={"/about"}>
             <li className="hidden sm:inline text-slate-700 hover:underline">
