02117321da7f3b6d02104c8d84eefe65b33700c1
apply007 <hasanat153@gmail.com>
2024-02-21 23:35:46 +0600

    create listing

--- Api/index.js
+++ Api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 
 import cookieParser from 'cookie-parser'
 
@@ -29,6 +30,7 @@ app.use(cookieParser())
 app.use("/api/user",userRouter)
 
 app.use("/api/auth",authRouter)
+app.use("/api/listing",listingRouter)
 
 
 
