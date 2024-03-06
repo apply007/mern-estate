8ee15beaac041c9034f4a53e1ba51f8b283ebae2
apply007 <hasanat153@gmail.com>
2024-03-07 00:26:48 +0600

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-80 max-w-[6xl] mx-auto">
+    <h1 className="text-3xl font-bold mb-4 text-slate-600 class">About Ashif Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio magnam
+        perferendis totam blanditiis quam pariatur ipsa temporibus voluptatibus!
+        Quaerat ipsam ratione dolor laudantium expedita sint ducimus
+        reprehenderit hic praesentium veritatis!
+      </p>
+      <p className="mb-4 text-slate-700" >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio magnam
+        perferendis totam blanditiis quam pariatur ipsa temporibus voluptatibus!
+        Quaerat ipsam ratione dolor laudantium expedita sint ducimus
+        reprehenderit hic praesentium veritatis!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio magnam
+        perferendis totam blanditiis quam pariatur ipsa temporibus voluptatibus!
+        Quaerat ipsam ratione dolor laudantium expedita sint ducimus
+        reprehenderit hic praesentium veritatis!
+      </p>
+    </div>
+  );
 }
