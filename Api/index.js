d67f6afeedd585c590c8168ef2f0e9fb01d7eeb4
apply007 <hasanat153@gmail.com>
2024-03-07 00:44:21 +0600

    add pre installation

--- Api/index.js
+++ Api/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from "path";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import listingRouter from './routes/listing.route.js'
-
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 mongoose
@@ -17,31 +17,37 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+ const _dirname=path.resolve()
+
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server  is At Running at port 3000");
 });
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter);
 
-app.use("/api/auth",authRouter)
-app.use("/api/listing",listingRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(_dirname,'/client/dist')))
 
-
-app.use((err,req,res,next)=>{
-  const statusCode=err.statusCode||500
-  const message=err.message||"Internal server Error"
-
-return res.status(statusCode).json({
-  success:false,
-  message,
-  statusCode
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(_dirname,'client','dist','index.html'))
 })
 
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
